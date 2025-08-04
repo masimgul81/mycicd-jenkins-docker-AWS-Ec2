fab0096ecb7c43768e1da571dd261d51bc3495d8
Asim Gul <asimgul@gmail.com>
2025-08-04 17:37:12 +0500

    agent2 commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Paa Jee 2025 - agent up2';
+    return 'Hello World! Mohsin paa jee';
   }
 }
